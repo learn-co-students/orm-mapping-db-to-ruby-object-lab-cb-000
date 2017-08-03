038303297f55a4989b09d6e742e550135383a16c
cptbrkfst <dyl@resonategroup.ca>
2017-08-03 17:30:10 -0400

    completed lab

--- lib/student.rb
+++ lib/student.rb
@@ -2,6 +2,50 @@ require 'pry'
 class Student
   attr_accessor :id, :name, :grade
 
+  def self.count_all_students_in_grade_9
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = 9;
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade < 12;
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.first_x_students_in_grade_10(limit)
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = 10 LIMIT ?;
+    SQL
+    DB[:conn].execute(sql, limit).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = 10 LIMIT 1;
+    SQL
+    row = DB[:conn].execute(sql).first
+    self.new_from_db(row)
+  end
+
+  def self.all_students_in_grade_x(grade)
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = ?;
+    SQL
+    DB[:conn].execute(sql, grade).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
   def self.new_from_db(row)
     new_student = self.new
     new_student.id = row[0]
